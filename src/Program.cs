0e1cabc148381340d6080a8abe15e363a2782827
Michal Pokorny <michal.pokorny@smarty.cz>
2026-06-21 12:55:36 +0200

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,16 @@
-Console.Write("$ ");
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+while (!cts.Token.IsCancellationRequested)
+{
+    Console.Write("$ ");
+
+    var command = await Console.In.ReadLineAsync();
+
+    Console.WriteLine($"{command}: command not found");
+}
