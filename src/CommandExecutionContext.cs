1e737f34b0ddf58c0840f67aeda49a07b47a9c5e
Michal Pokorny <michal.pokorny0@gmail.com>
2026-06-21 20:00:14 +0200

    codecrafters submit [skip ci]

---
+++ src/CommandExecutionContext.cs
@@ -0,0 +1,69 @@
+﻿public record CommandExecutionContext(string[] Args, IReadOnlyCollection<Command> AllCommands)
+{
+    public bool IsHaltRequested { get; private set; }
+
+    public Command? GetCommand(string command)
+    {
+        if (GetKnownCommand(command) is { } kc)
+        {
+            return kc;
+        }
+
+        foreach (var d in FileSystemHelper.GetPathDirectories())
+        {
+            var c = d.EnumerateFiles($"{command}.exe").FirstOrDefault();
+
+            if (c is null)
+            {
+                continue;
+            }
+
+            if (c.IsReadOnly)
+            {
+                continue;
+            }
+
+            return new PathFileCommand(c, ctx =>
+            {
+                // TODO
+
+                return Task.CompletedTask;
+            });
+        }
+
+        return null;
+    }
+
+
+    public Command? GetKnownCommand(string command)
+    {
+        return this.AllCommands.FirstOrDefault(x => command.Equals(x.CommandName, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public void Halt()
+    {
+        this.IsHaltRequested = true;
+    }
+}
+
+public static class FileSystemHelper
+{
+    public static IEnumerable<DirectoryInfo> GetPathDirectories()
+    {
+        var env = Environment.GetEnvironmentVariable("PATH") ?? string.Empty;
+
+        var dirs = env.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        foreach (var dir in dirs)
+        {
+            var d = new DirectoryInfo(dir);
+
+            if (!d.Exists)
+            {
+                continue;
+            }
+
+            yield return d;
+        }
+    }
+}
---
+++ src/DescriptionExtensions.cs
@@ -0,0 +1,10 @@
+﻿public static class DescriptionExtensions
+{
+    extension(CommandType type)
+    {
+        public string GetDescription(string command) => type switch
+        {
+            CommandType.ShellBuilin => $"{command} is a shell builtin",
+        };
+    }
+}
