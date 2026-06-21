94d135d00fe22c5050dc8d9b5d9a235f1fa94603
Michal Pokorny <michal.pokorny@smarty.cz>
2026-06-21 18:04:05 +0200

    CLI tests (cli-test-1782057844905)

--- Commands/TypeCommand.cs
+++ Commands/TypeCommand.cs
@@ -23,3 +23,27 @@ public record TypeCommand() : Command("type")
         }
     }
 }
+
+public record BashCommand() : Command("bash")
+{
+    public override async Task Handle(CommandExecutionContext ctx)
+    {
+        var what = ctx.Args.FirstOrDefault() ?? "";
+
+        var c = ctx.GetCommand(what);
+
+        if (c is null)
+        {
+            await ctx.StdOut.WriteLineAsync($"{what}: not found");
+        }
+        else if (c is PathFileCommand pf)
+        {
+            await ctx.StdOut.WriteLineAsync($"{what} is {pf.File.FullName}");
+        }
+        else
+        {
+            // assumes shell
+            await ctx.StdOut.WriteLineAsync($"{what} is a shell builtin");
+        }
+    }
+}
