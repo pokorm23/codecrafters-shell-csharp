98f4b4376e6bb3d9ab66314dc82d513f85c7c38c
Michal Pokorný <michal.pokorny0@gmail.com>
2026-08-30 19:05:06 +0200

    codecrafters submit [skip ci]

--- Commands/HistoryCommand.cs
+++ Commands/HistoryCommand.cs
@@ -4,7 +4,15 @@ public record HistoryCommand() : Command("history")
 {
     public override async Task Handle(CommandExecutionContext ctx)
     {
-        foreach (var (i, m) in CommandHistoryStore.Commands.Index())
+        int? n = int.TryParse(ctx.Args.FirstOrDefault() ?? "", out var ii) ? ii : null;
+        
+        var toTake = !n.HasValue
+                     || n.Value > CommandHistoryStore.Commands.Count
+                     || n.Value < 0
+                     ? CommandHistoryStore.Commands.Count
+                     : n.Value;
+        
+        foreach (var (i, m) in CommandHistoryStore.Commands.Index().TakeLast(toTake))
         {
             await ctx.StdOut.WriteLineAsync($"    {i+1} {m}");
         }
